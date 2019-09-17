92c6c9193db02630f854752fe5c4fb0078398cdd
Vladimir Pavković <vladimirpavk@gmail.com>
2019-09-17 14:13:03 +0200

    +

--- app.ts
+++ app.ts
@@ -6,34 +6,27 @@ import { MainRouterClass } from './router/main.router';
 
 export class AppClass{
     private _expressServer: express.Express;
+    private _httpServer: httpServer.Server;
     private _port:number=3080;
 
     constructor(){
         console.log('Application started...');
 
-        this._expressServer = express();
-
-        let _httpServer:httpServer.Server = new httpServer.Server(this._expressServer);
-        let webSocket = io(_httpServer);
-
-        webSocket.on('connection',
-            (s:io.Socket)=>{
-                console.log('socket.io connection...', s);
-            }
-        );
-
-        let mainRouter:MainRouterClass=new MainRouterClass();
-        this._expressServer.use(mainRouter.router);
-
-        this._startServer();
-    }
-
-    private _startServer(){
-        this._expressServer.listen(
+        this._expressServer = express();        
+        let httpServer = this._expressServer.listen(
             this._port,
             ()=>{
                 console.log('Server is listening on port ', this._port);
+                let webSocket = io(this._httpServer);
+                webSocket.on('connection',
+                    (s:io.Socket)=>{
+                        console.log('socket.io connection...', s);
+                    }
+                );
             }
-        )
-    }
+        )       
+
+        let mainRouter:MainRouterClass=new MainRouterClass();
+        this._expressServer.use(mainRouter.router);        
+    }    
 }
